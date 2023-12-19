dbdb304bdeee66dbac457b404b1f4b37d32ad45d
John <karumanx003@gmail.com>
2023-12-19 16:50:54 +0700

    finish bro

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    string nstr;
+
+    double m,sd = 0,sum = 0,sumx,i=0;
+    ifstream source;
+    source.open("score.txt");
+    while(getline(source,nstr)){
+        sum += atof(nstr.c_str());
+        sumx = atof(nstr.c_str());
+        sd += pow(sumx,2);
+        i += 1;
+    }
+    m = sum / i;
+    
+
+
+        cout << "Number of data = " << i ;
+        cout << setprecision(3);
+        cout << "\nMean = " << m;
+         cout << setprecision(3);
+        cout << "\nStandard deviation = "<< sqrt((sd/i) - pow(m,2));
+}
